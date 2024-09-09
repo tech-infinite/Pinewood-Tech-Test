56f815c24e2b739e76277e390243dbc18937a166
Supraja Ramaraju <test.email@example.com>
2024-09-09 16:20:48 +0100

    Simplified name property in the Customer Model class

--- CustomersAPI/Models/Customer.cs
+++ CustomersAPI/Models/Customer.cs
@@ -3,9 +3,7 @@
     public class Customer
     {
         public int CustomerID { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-
+        public string? Name { get; set; }
         public string? Address { get; set; }
 
         public string? Phone { get; set; }
