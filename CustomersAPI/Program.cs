b0969933feddcc056a6c26f19d0b06e89bd84c05
Supraja Ramaraju <test.email@example.com>
2024-09-10 21:46:17 +0100

    Change to Program.cs file

--- CustomersAPI/Program.cs
+++ CustomersAPI/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
